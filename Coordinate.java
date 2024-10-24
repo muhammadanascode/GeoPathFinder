5191ca0483f5a3020cfb2d96d343d14bbbf46b09
muhammadanascode <anassohal34343@gmail.com>
2024-10-24 12:15:42 +0500

    Created data fields for longitude and latitude and created constructor and getters has been made for longitude and latitude

---
+++ Coordinate.java
@@ -0,0 +1,17 @@
+class Coordinnate {
+    double longitude;
+    double latitude;
+
+    public Coordinnate(double longitude, double latitude) {
+        this.longitude = longitude;
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return this.longitude;
+    }
+
+    public double latitude() {
+        return this.latitude;
+    }
+}
